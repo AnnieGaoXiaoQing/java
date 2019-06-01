ce6df99249aabfbf3f3c735f2b04a613c179687f
gaoxiaoqing <gxq741718618@163.com>
2019-06-01 20:25:59 +0800

    初始化

---
+++ basic/src/TestVariable.java
@@ -0,0 +1,14 @@
+/**
+ * 测试变量
+ * Created by gxq74_000 on 2019/6/1.
+ */
+public class TestVariable {
+
+    int a; // 成员变量，从属于对象；成员变量会自动初始化
+    static int size; //静态变量，从属于类
+
+    public static void main(String[] args) {
+        int balance = 4; // 局部变量（方法）
+    }
+
+}
